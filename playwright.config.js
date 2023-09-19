da40ec9463b73347b8fbf47dd0b0f12f90b99eda
betsywambui <bethwambuimuniu@gmail.com>
2023-09-20 01:51:58 +0300

    add package-lock.json to gitignore

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,9 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 import dotenv from 'dotenv';
+import path from 'path'
 
-
-dotenv.config({path: '../.env'});
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
